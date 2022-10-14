d9af22f7c5bc8597401b5f0e7501f36d6a0794b9
Gustavo Zaniboni <gustavo@Gustavos-iMac.local>
2022-10-14 00:05:38 -0300

    tests naive

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -18,4 +18,36 @@ class ExampleTest extends TestCase
 
         $response->assertStatus(200);
     }
+
+    public function test_api_request_get_comments()
+    {
+        $response = $this->getJson('/api/comments');
+ 
+        $response
+            ->assertStatus(200);
+    }
+    public function test_api_request_create_new_comment()
+    {
+        $response = $this->putJson('/api/comments/', ['name'=>'Teste', 'comment'=>'teste comment', 'parent_id'=>1]);
+ 
+        $response
+            ->assertStatus(200);
+    }
+
+    public function test_api_request_update_comment()
+    {
+        $response = $this->putJson('/api/comments/{id}/', ['name'=>'Teste', 'comment'=>'teste comment']);
+ 
+        $response
+            ->assertStatus(200);
+    }
+
+    public function test_api_request_delete_comment()
+    {
+        $response = $this->deleteJson('/api/comments/1');
+ 
+        $response
+            ->assertNoContent();
+      // verify database
+    }
 }
